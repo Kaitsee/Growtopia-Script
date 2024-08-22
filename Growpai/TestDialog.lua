49271005e13963eeeb97a58edb59b8e7269764ba
Kaitsee <fikri.pangestu1005@gmail.com>
2024-08-22 13:54:33 +0700

    Update TestDialog.lua

--- Growpai/TestDialog.lua
+++ Growpai/TestDialog.lua
@@ -7,6 +7,7 @@ add_checkbox|spamcolor|`2Enable `9Colored Text|0|
 add_checkbox|spamemote|`9Add Emote Spamming|0|
 add_checkbox|spampulloff|`4Disable `9Auto Spam When You Pull Someone|
 add_spacer|small|
+add_custom_margin|x:8;y:-30|
 add_text_input|spamtext|`9Spam Text :|Hello|120|
 add_custom_textbox|`9Insert Spam Text|size:small|
 add_text_input|spamdelay|`9Interval (ms) :|4000|10|
